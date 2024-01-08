df9de67356d67cfaf4fbb19e743958a9f844e38e
Hunaidshakoor <hshakoor3@gatech.edu>
2024-01-08 14:02:09 -0800

    Part1 edit

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,31 +1,6 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 
-// const Hello = (props) => {
-//   console.log(props)
-//   return (
-//     <div>
-//       <p>
-//         Hello {props.name}, you are {props.age} years old
-//       </p>
-//     </div>
-//   )
-// }
-
-// const App = () => {
-//   const name = 'Peter'
-//   const age = 10
-
-//   return (
-//     <div>
-//       <h1>Greetings</h1>
-//       <Hello name='Maya' age={26 + 10} />
-//       <Hello name={name} age={age} />
-//     </div>
-//   )
-// }
-
-
 const Header = (props) => {
   return (
     <h1>
